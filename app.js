9c1740abad94e510e37f8b3cff2772bfd3dd29db
rf222cz <rf222cz@student.lnu.se>
2015-11-19 17:04:11 +0100

    can get API :frog:

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 "use strict";
-  var express = require('express');
-  var app = express();
+  var express = require('express'),
+  app = express(),
+  request = require('request');
 
   app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
+
+    var url = 'http://opencontent.infomaker.io:8080/opencontent/health';
+
+    request(url, function (error, response, body) {
+    	if (!error) {
+    		console.log(body);
+    	} else {
+    		console.log("We’ve encountered an error: " + error);
+    	}
+    });
+
   });
 
   var server = app.listen(8080, function () {
