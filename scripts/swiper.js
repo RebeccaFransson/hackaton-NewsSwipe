e2da87256c8c08136851f9803b66796eaf5e3e50
hj222hi <hj222hi@student.lnu.se>
2015-11-19 14:28:59 +0100

    jTInder :kiss: + mer test :beer:

---
+++ scripts/swiper.js
@@ -0,0 +1,23 @@
+function init() {
+  $("#tinderslide").jTinder({
+    onDislike: function (item) {
+        alert('Dislike image ' + (item.index()+1));
+    },
+    onLike: function (item) {
+        alert('Like image ' + (item.index()+1));
+    },
+    animationRevertSpeed: 200,
+    animationSpeed: 400,
+    threshold: 1,
+    likeSelector: '.like',
+    dislikeSelector: '.dislike'
+  });
+}
+
+window.onload = function() {
+  init();
+
+};
+
+function DisplayArticle(ArticleID) {
+  }
