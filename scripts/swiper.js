101dc94b902c6201faadb614c7c31024bfa4e377
hj222hi <hj222hi@student.lnu.se>
2015-11-19 17:33:39 +0100

    Nu så vissas en artikel

--- scripts/swiper.js
+++ scripts/swiper.js
@@ -5,7 +5,8 @@ function init() {
     },
     onLike: function (item) {
         //alert('Like image ' + (item.index()+1));
-        alert("It's a like");
+        //alert("It's a like");
+        DisplayArticle(1);
     },
     animationRevertSpeed: 200,
     animationSpeed: 400,
@@ -21,6 +22,27 @@ window.onload = function() {
 };
 
 function DisplayArticle(ArticleID) {
+  var articleDiv = document.createElement("div");
+  var articleHeader = document.createElement("h1");
+  var articleText = document.createElement("p");
+  var articleImage = document.createElement("img");
+    var stringwithwords = "Fromage paneer blue castello. Say cheese pecorino goat fondue cheddar red leicester queso brie. Jarlsberg caerphilly stilton dolcelatte say cheese pecorino port-salut monterey jack. Babybel st. agur blue cheese cheese triangles. Cheesy grin dolcelatte bocconcini. Jarlsberg macaroni cheese pecorino melted cheese rubber cheese cauliflower cheese dolcelatte cheddar. Croque monsieur melted cheese stinking bishop red leicester emmental smelly cheese edam squirty cheese. Macaroni cheese mascarpone ricotta cheese and biscuits cottage cheese say cheese the big cheese cottage cheese. Red leicester cheese triangles stinking bishop.    Cheesy grin dolcelatte bocconcini. Jarlsberg macaroni cheese pecorino melted cheese rubber cheese cauliflower cheese dolcelatte cheddar. Croque monsieur melted cheese stinking bishop red leicester emmental smelly cheese edam squirty cheese. Macaroni cheese mascarpone ricotta cheese and biscuits cottage cheese say cheese the big cheese cottage cheese. Red leicester cheese triangles stinking bishop."
+
+  articleDiv.setAttribute("id", "articlediv");
+  articleHeader.setAttribute("id", "articleheader");
+  articleText.setAttribute("id", "articletext");
+  articleImage.setAttribute("id", "articleimage");
+
+
+  document.body.appendChild(articleDiv);
+  document.getElementById("articlediv").appendChild(articleHeader);
+  document.getElementById("articlediv").appendChild(articleImage);
+  document.getElementById("articlediv").appendChild(articleText);
+
+  document.getElementById("articleheader").innerHTML = "works";
+  document.getElementById("articletext").innerHTML = stringwithwords;
+  document.getElementById("articleimage").setAttribute("href", "pics/3.jpg");
+  //document.getElementById("articletext").innerHTML = "works";
   //document.getElementById("Article").className = "show";
   //document.getElementByTagName("body").className = "showFullPage";
 
