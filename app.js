36f3b5e9ee5c8e380118666c049795f922991e5d
rf222cz <rf222cz@student.lnu.se>
2015-11-19 14:27:50 +0100

    Uppe lokalt :house:

--- app.js
+++ app.js
@@ -3,10 +3,10 @@
   var app = express();
 
   app.get('/', function (req, res) {
-    res.send('Hello World!');
+    res.sendFile(__dirname + '/index.html');
   });
 
-  var server = app.listen(3000, function () {
+  var server = app.listen(8080, function () {
     var host = server.address().address;
     var port = server.address().port;
 
