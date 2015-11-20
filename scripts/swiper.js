0419820791255be55c483d3cb25a21e6f1f22357
hj222hi <hj222hi@student.lnu.se>
2015-11-20 09:56:40 +0100

    :beers:

--- scripts/swiper.js
+++ scripts/swiper.js
@@ -1,13 +1,13 @@
-var i = 0;
+var i = 5;
 //var load = ;
 
 function init(object) {
-DisplayArticleStart(object[i]);
+DisplayArticleStart(object);
   $("#newsBox").jTinder({
     onDislike: function (item) {
-      loadDoc();
-      //DisplayArticleStart(object[i]);
-      i++;  //alert("It's a dislike");
+      //loadDoc();
+      //DisplayArticleStart(object);
+      i -= 1;  //alert("It's a dislike");
     },
     onLike: function (item) {
         //alert('Like image ' + (item.index()+1));
@@ -15,8 +15,8 @@ DisplayArticleStart(object[i]);
 
         DisplayArticle(object[i]);
         console.log(object[i]);
-        i++;
-        //DisplayArticleStart(object[i]);
+        i -= 1;
+
     },
     animationRevertSpeed: 200,
     animationSpeed: 400,
@@ -70,8 +70,19 @@ function loadDoc() {
 }
 
 function DisplayArticleStart(Article) {
-  document.querySelector('#pic3').setAttribute("src", Article.image);
-  document.querySelector('#texten3').innerHTML = Article.headline;
+  console.log(Article);
+  document.querySelector('#pic1').setAttribute("src", Article[0].image);
+  document.querySelector('#texten1').innerHTML = Article[0].headline;
+  document.querySelector('#pic2').setAttribute("src", Article[1].image);
+  document.querySelector('#texten2').innerHTML = Article[1].headline;
+  document.querySelector('#pic3').setAttribute("src", Article[2].image);
+  document.querySelector('#texten3').innerHTML = Article[2].headline;
+  document.querySelector('#pic4').setAttribute("src", Article[3].image);
+  document.querySelector('#texten4').innerHTML = Article[3].headline;
+  document.querySelector('#pic5').setAttribute("src", Article[4].image);
+  document.querySelector('#texten5').innerHTML = Article[4].headline;
+  document.querySelector('#pic6').setAttribute("src", Article[5].image);
+  document.querySelector('#texten6').innerHTML = Article[5].headline;
   // document.querySelector('#pic3').setAttribute("src", Article.image);
   // document.querySelector('#texten3').innerHTML = Article.headline;
   // document.querySelector('#pic3').setAttribute("src", Article.image);
